12b2b691bb87f584963a6bdb0b4cd49613f072c2
daily1mostafa <daily1mostafa@gmail.com>
2025-12-06 21:31:17 +0200

    backtracking function defintions

---
+++ backtrackingAlgorithm.py
@@ -0,0 +1,48 @@
+def sortItems(itemsList):
+
+    pass
+
+
+def placeItem(item, bin, binCapacity=None):
+
+    pass
+
+
+def removeItem(item, bin, binCapacity=None):
+
+    pass
+
+
+def calculateLowerBound(remainingItems, binCapacity):
+
+    pass
+
+
+def copyBins(bins):
+
+    pass
+
+
+def pruneBranch(usedBins, bestSolution, remainingItems, binCapacity):
+
+    pass
+
+
+def initializeSolution(items, binCapacity):
+
+    pass
+
+
+def findFirstFitBin(item, currentBins, binCapacity):
+
+    pass
+
+
+def backtrack(currentIndex, usedBins, binCapacity, bestSolution, sortedItemsList):
+
+    pass
+
+
+def solveBinPacking(items, binCapacity):
+
+    pass
